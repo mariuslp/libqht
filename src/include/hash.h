39a437b44ec7aa217ec0749ac3314fb60cf7bd8e
Marius Lombard-Platet <marius.lombard-platet@ens.fr>
2019-01-23 10:44:56 +0000

    Remove useless parts

--- src/include/hash.h
+++ src/include/hash.h
@@ -22,26 +22,3 @@ template<class T> HashValue Hash2(T t) {
 	return seed;
 }
 
-HashValue Hash3(const HashValue t);
-
-template<class T> std::vector<HashValue> getHashes(const size_t k, const T& t, const size_t m) {
-	/** Generates k hashes for an element t
-	 *   @param k: number of hashes to generate
-	 *   @param t: object to be hashed
-	 *   @param m: the size of the hashes
-	 *
-	 *   @returns A family of HashValues which are supposed to have a similar influence than independent hashes
-	 *   @see "Less Hashing, Same Performance: Building a Better Bloom Filter" by A. Kirsch & M. Mitzenmacher
-	 */
-	std::vector<HashValue> results;
-
-	HashValue hash1 = Hash1<T>(t);
-	HashValue hash2 = Hash2<T>(t);
-
-	results.push_back(hash1 % m);
-	for(size_t i=1; i<k; ++i) {
-		results.push_back((results[i - 1] + hash2) % m);
-	}
-
-	return results;
-}
