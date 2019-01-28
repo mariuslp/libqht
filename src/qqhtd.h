c77c9fe4cc35dc9d7c1b4e8a7217aed759c71341
Marius Lombard-Platet <marius.lombard-platet@ens.fr>
2019-01-28 14:07:38 +0100

    clean qqhtd

--- src/qqhtd.h
+++ src/qqhtd.h
@@ -7,84 +7,90 @@ template <class T> struct QQHTDFilter : QHTFilter<T> {
 public:
 	QQHTDFilter(const uint64_t memory_size, const size_t n_n_buckets, const size_t n_fingerprint_size);
 	bool Insert(const T& e);
-        bool Delete(const T& e);
+	bool Delete(const T& e);
 
 protected:
-    bool InsertFingerprintInLastBucket(const uint64_t address, const uint64_t fingerprint);
+	bool InsertFingerprintInLastBucket(const size_t address, const uint64_t fingerprint);
 };
 
-template <class T> QQHTDFilter<T>::QQHTDFilter(const uint64_t memory_size, const size_t n_n_buckets, const size_t n_fingerprint_size) : QHTFilter<T>(memory_size, n_n_buckets, n_fingerprint_size) {
+template <class T> QQHTDFilter<T>::QQHTDFilter(
+	const uint64_t memory_size,
+	const size_t n_n_buckets,
+	const size_t n_fingerprint_size
+) : QHTFilter<T>(memory_size, n_n_buckets, n_fingerprint_size) {
 }
 
 template <class T> bool QQHTDFilter<T>::Insert(const T& e) {
 	/**
 	 * Inserts element e in the filter
-         * @param e
-         * @return bool : true if e is detected as a duplicate, false otherwise 
+	 * @param e
+	 * @return bool : true if e is detected as a duplicate, false otherwise
 	 */
-        auto detected = this->Lookup(e);
+	auto detected = this->Lookup(e);
 
-        size_t address = Hash1(e) % this->n_cells;
-        auto fingerprint = this->Fingerprint(e);
+	auto address = this->Address(e);
+	auto fingerprint = this->Fingerprint(e);
 
-        InsertFingerprintInLastBucket(address, fingerprint);
+	InsertFingerprintInLastBucket(address, fingerprint);
 
-        return true;
+	return true;
 }
 
-template <class T> bool QQHTDFilter<T>::InsertFingerprintInLastBucket(const uint64_t address, const uint64_t fingerprint) {
-        /**
-         * In QQHTD, buckets behave like a queue. Therefore each element is inserted at the end of the queue.
-         * Using a linked list would require additional bits of data (for storing pointers).
-         * Therefore we manually reorder the buckets in the insertion.
-         *
-         * @param size_t address
-         * @param uint64_t fingerprint
-         *
-         * @returns bool true
-         */
-        for(size_t i = 0; i < this->n_buckets - 1; ++i) {
-                this->InsertFingerprintInBucket(address, i, this->GetFingerprintFromBucket(address, i + 1));
-        }
+template <class T> bool QQHTDFilter<T>::InsertFingerprintInLastBucket(const size_t address, const uint64_t fingerprint) {
+	/**
+	 * In QQHTD, buckets behave like a queue. Therefore each element is inserted at the end of the queue.
+	 * Using a linked list would require additional bits of data (for storing pointers).
+	 * Therefore we manually reorder the buckets in the insertion.
+	 *
+	 * @param size_t address
+	 * @param uint64_t fingerprint
+	 *
+	 * @returns bool true
+	 */
+	for(size_t i = 0; i < this->n_buckets - 1; ++i) {
+		this->InsertFingerprintInBucket(address, i, this->GetFingerprintFromBucket(address, i + 1));
+	}
 
-        this->InsertFingerprintInBucket(address, this->n_buckets - 1, fingerprint);
+	this->InsertFingerprintInBucket(address, this->n_buckets - 1, fingerprint);
 
-        return true;
+	return true;
 }
 
 template <class T> bool QQHTDFilter<T>::Delete(const T& e) {
-        /**
-         * Overrides QHT's Delete method.
-         * Same principle applies
-         *
-         * @param T e : the element to be deleted
-         * @returns bool : true if deleted, false if not found
-         */
+	/**
+	 * Overrides QHT's Delete method.
+	 * Same principle applies
+	 *
+	 * @param T e : the element to be deleted
+	 * @returns bool : true if deleted, false if not found
+	 */
 
-        bool element_found = false;
+	bool element_found = false;
 
-        size_t address = Hash1(e) % this->n_cells;
-        auto fingerprint = this->Fingerprint(e);
+	auto address = this->Address(e);
+	auto fingerprint = this->Fingerprint(e);
 
-        size_t i = 0;
-        while(! element_found && i < this->n_buckets) {
-                if(this->GetFingerprintFromBucket(address, i) == fingerprint) {
-                        element_found = true;
-                        // InsertFingerprintInBucket(address, i, 0);
-                } else {
-                        ++i;
-                }
-        }
+	size_t i = 0;
+	while(! element_found && i < this->n_buckets) {
+		if(this->GetFingerprintFromBucket(address, i) == fingerprint) {
+			element_found = true;
+		} else {
+			++i;
+		}
+	}
 
-        if(! element_found) {
-                return false;
-        }
+	if(! element_found) {
+		return false;
+	}
 
-        for(; i < this->n_buckets - 1; ++i) {
-                this->InsertFingerprintInBucket(address, i, this->GetFingerprintFromBucket(address, i + 1));
-        }
+	// Remove the element from the list by shifting the following elements one cell to the left
+	for(; i < this->n_buckets - 1; ++i) {
+		this->InsertFingerprintInBucket(address, i, this->GetFingerprintFromBucket(address, i + 1));
+	}
 
-        this->InsertFingerprintInBucket(address, this->n_buckets - 1, 0);
+	// Re-set the last element of the queue to `Empty`. Also covers the case where the e to be removed
+	// is the last element of the queue.
+	this->InsertFingerprintInBucket(address, this->n_buckets - 1, 0);
 
-        return true;
+	return true;
 }
